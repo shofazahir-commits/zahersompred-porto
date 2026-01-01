6373dcdf7205ede238856baf98a7024346c88edd
shofazahir-commits <shofazahir@gmail.com>
2026-01-01 19:32:27 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const reveals = document.querySelectorAll(".reveal");
+
+let lastScrollY = window.scrollY;
+let scrollDirection = "down";
+
+window.addEventListener("scroll", () => {
+    scrollDirection = window.scrollY > lastScrollY ? "down" : "up";
+    lastScrollY = window.scrollY;
+});
+
+const observer = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.remove("from-top", "from-bottom");
+
+                if (scrollDirection === "down") {
+                    entry.target.classList.add("from-bottom");
+                } else {
+                    entry.target.classList.add("from-top");
+                }
+
+                entry.target.classList.add("active");
+            } else {
+                // supaya bisa muncul berulang kali
+                entry.target.classList.remove("active");
+            }
+        });
+    },
+    {
+        threshold: 0.15
+    }
+);
+
+reveals.forEach(el => observer.observe(el));
